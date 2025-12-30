1aa18918c0ea4e0befd0b6f137b77292edf52dae
franklin <franklin@alphasoftz.in>
2025-12-30 21:15:51 +0530

    footer changes

--- resources/views/front_base/footer.blade.php
+++ resources/views/front_base/footer.blade.php
@@ -107,82 +107,37 @@
 
    <!-- Start of Sticky Footer -->
    <div class="sticky-footer sticky-content fix-bottom">
-       <a href="demo1.html" class="sticky-link active">
+       <a  href="{{ url('demoEight') }}" class="sticky-link active">
            <i class="w-icon-home"></i>
            <p>Home</p>
        </a>
-       <a href="shop-banner-sidebar.html" class="sticky-link">
+       <a href="{{ url('vendorDokenGrid') }}" class="sticky-link">
            <i class="w-icon-category"></i>
            <p>Shop</p>
        </a>
-       <a href="my-account.html" class="sticky-link">
-           <i class="w-icon-account"></i>
-           <p>Account</p>
-       </a>
-       <div class="cart-dropdown dir-up">
-            <a href="cart.html" class="sticky-link">
+
+        <?php  if(session('customer_id')){ ?>
+
+            <a href="{{ route('myAccount') }}" class="sticky-link">
+                <i class="w-icon-account"></i>
+                <p>Account</p>
+            </a>
+
+        <?php  }else{ ?>
+
+            <a  onclick="showLoginPopup()"   class="sticky-link">
+                <i class="w-icon-account"></i>
+                <p>Login</p>
+            </a>
+
+       <?php } ?>
+
+            <a  href="javascript:void(0)" onclick="showSideCart()" class="cart-toggle label-down sticky-link "    >
                <i class="w-icon-cart"></i>
                <p>Cart</p>
            </a>
-           <div class="dropdown-box">
-               <div class="products">
-                   <div class="product product-cart">
-                       <div class="product-detail">
-                           <h3 class="product-name">
-                               <a href="product-default.html">Beige knitted elas<br>tic
-                                   runner shoes</a>
-                           </h3>
-                           <div class="price-box">
-                               <span class="product-quantity">1</span>
-                               <span class="product-price">$25.68</span>
-                           </div>
-                       </div>
-                       <figure class="product-media">
-                           <a href="#">
-                               <img src="<?= asset('frontend') ?>/images/cart/product-1.jpg" alt="product" height="84"
-                                   width="94" />
-                           </a>
-                       </figure>
-                       <button class="btn btn-link btn-close" aria-label="button">
-                           <i class="fas fa-times"></i>
-                       </button>
-                   </div>
-
-                   <div class="product product-cart">
-                       <div class="product-detail">
-                           <h3 class="product-name">
-                               <a href="product.html">Blue utility pina<br>fore
-                                   denim dress</a>
-                           </h3>
-                           <div class="price-box">
-                               <span class="product-quantity">1</span>
-                               <span class="product-price">$32.99</span>
-                           </div>
-                       </div>
-                       <figure class="product-media">
-                           <a href="#">
-                               <img src="<?= asset('frontend') ?>/images/cart/product-2.jpg" alt="product" width="84"
-                                   height="94" />
-                           </a>
-                       </figure>
-                       <button class="btn btn-link btn-close" aria-label="button">
-                           <i class="fas fa-times"></i>
-                       </button>
-                   </div>
-               </div>
-
-               <div class="cart-total">
-                   <label>Subtotal:</label>
-                   <span class="price">$58.67</span>
-               </div>
-
-               <div class="cart-action">
-                   <a href="cart.html" class="btn btn-dark btn-outline btn-rounded">View Cart</a>
-                   <a href="checkout.html" class="btn btn-primary  btn-rounded">Checkout</a>
-               </div>
-           </div>
-           <!-- End of Dropdown Box -->
-       </div>
+       
+        
 
        <div class="header-search hs-toggle dir-up">
            <a href="#" class="search-toggle sticky-link">
@@ -247,7 +202,7 @@
                                        @if(count($categoriesmain->submenu) > 0)    
                        <li>
                            <a  href="{{ url( 'mainCategoryShop/'.$categoriesmain->id ) }}">
-                               <i class="w-icon-tshirt2"></i>   {{ $categoriesmain->category_main_name }}
+                                 {{ $categoriesmain->category_main_name }}
                            </a>
                            <ul>
                               @foreach($categoriesmain->submenu as $submenus)                                                
